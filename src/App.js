d129db967f6ec1feee1b4762aefd418f0a32dd24
Hasan Polat <hasnpol2002@gmail.com>
2024-06-22 22:10:49 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hasan Emin Polat
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
